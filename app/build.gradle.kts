111d427e5549372bfe3b9abcf78521736145dfc0
User662122 <gwkrishna06@gmail.com>
2025-11-05 02:18:53 +0530

    Fix build: Remove OpenCV dependencies, use simple image processing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,16 +45,5 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     
-    // OpenCV
-    implementation("org.opencv:opencv:4.10.0")
-    
-    // Kotlin scripting
-    implementation("org.jetbrains.kotlin:kotlin-scripting-jvm:1.9.20")
-    implementation("org.jetbrains.kotlin:kotlin-script-runtime:1.9.20")
-    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.9.20")
-    implementation("org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:1.9.20")
-    
-    // Code editor
-    implementation("io.github.rosemoe.sora-editor:editor:0.23.2")
-    implementation("io.github.rosemoe.sora-editor:language-textmate:0.23.2")
+    // Note: OpenCV needs to be added manually - see README.md for instructions
 }
